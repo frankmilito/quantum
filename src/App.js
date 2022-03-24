3d8f8eb085b8ff2554deeca6155c15cba3355be7
Franklin Obidike <Franklin@Obi-Franklin.local>
2022-03-24 01:50:09 +0100

    folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,81 @@
+import {useState} from "react"
+import Header from "./components/Header"
 function App() {
+  const API_KEY = process.env.REACT_API_KEY
+  const [nodata, setNodata] = useState("No data yet")
+  const [searchTerm, setSearchTerm] = useState("")
+  const [weatherData, setWeatherData] = useState([])
+  const [city, setCity] = useState("Unknown Location")
+  const [weatherIcon, setWeatherIcon] = useState(
+    `${process.env.REACT_APP_ICON}10n@2x.png`
+  )
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className="bg-grey-800 flex items-center w-screen h0screen py-10">
+      <div className="flex w-3/4 min-h-full rounded-3xl shadow-lg m-auto bg-gray-100">
+        <div className="form-container">
+          <div className="flex items-center justify-center">
+            <h3 className="my-auto mr-auto text-xl text-pink-800 font-bold shadow-md py-1 px-3 rounded--md bg-white bg-opacity-30">
+              Forcast
+            </h3>
+            <div className="flex p-2 text-gray-100 bg-gray-600 bg-opacity-30 rounded-lg">
+              <i className="fa fa-map my-auto" aria-hidden="true"></i>
+              <div className="text-right">
+                <p className="font-semibold text-sm ml-2">{city}</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-white text-2xl">Weather Forcast App</h1>
+            <hr className="h-1 bg-white w-1 rounded-full my-5" />
+            <form
+              noValiddate
+              onSubmit={handleSubmit}
+              className="flex justify-center w-full"
+            >
+              <input
+                className="relative rounded-xl py-2 px-3 w-2/3 bg-opacity-60 text-white placeholder-gray-200"
+                onChange={handleChange}
+                required
+              />
+              <button type="submit" className="z-10 ">
+                <i
+                  className="fa fa-search text-white ml-10 border-1 my-auto z-10 cursor-pointer p-3"
+                  aria-hidden="true"
+                  onClick={() => navigator.geolocation.getCurrentPosition(myIP)}
+                ></i>
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="w-2/4 p-5">
+          <Header />
+          <div className="flex flex-col my-10">
+            {weatherData.length === 0 ? (
+              <div className="container p-4 flex items-center justify-center h-1/3 mb-auto">
+                <h1 className="text-grey-300 text-4xl font-bold uppercase">
+                  {nodata}
+                </h1>
+              </div>
+            ) : (
+              <>
+                <h1 className="text-5xl text-grey-800 mt-auto mb-4">Today</h1>
+                <DetailCard weather_icon={weatherIcon} data={weatherData} />
+                <h1 className="text-3xl text-grey-600 mb-4 mt-10">
+                  More on {city}
+                </h1>
+                <ul className="grid grid-cols-2 gap-2">
+                  {weatherData.list.map((days, index) => {
+                    if (index > 0) {
+                      return <SummaryCard key={index} day={days} />
+                    }
+                  })}
+                </ul>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
